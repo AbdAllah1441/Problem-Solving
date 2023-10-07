5bc97ef3c01f839c263328571f230d5fd9e49bd8
عبدالله مصطفى <abdullahmostafa9957@gmail.com>
2023-10-08 02:45:54 +0300

    Create Binary Search Tree : Insertion.cpp

---
+++ HackerRank/ddBinary Search Tree : Insertion/Binary Search Tree : Insertion.cpp
@@ -0,0 +1,36 @@
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+
+
+Node * insert(Node * root, int data)
+{
+    if (!root)
+        root = new Node(data);
+    else
+    {
+        if (data > root -> data)
+        {
+            root -> right = insert(root -> right, data);
+        }
+        if (data < root -> data)
+        {
+            root -> left = insert(root -> left, data);
+        }
+    }
+    return root;
+}
