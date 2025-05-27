4d6c68c46c4ff3ec82f52820d1d40543815c3d4f
عبدالله مصطفى <abdullahmostafa9957@gmail.com>
2025-05-27 11:13:50 +0300

    Update ContainsDuplicate.js

--- LeetCode/Contains Duplicate/ContainsDuplicate.js
+++ LeetCode/Contains Duplicate/ContainsDuplicate.js
@@ -1,18 +1,16 @@
-class Solution {
-    /**
-     * @param {number[]} nums
-     * @return {boolean}
-     */
-    hasDuplicate(nums) {
-        let x = {};
-        for (let i = 0; i < nums.length; i++) {
-            if (x[`${nums[i]}`]) {
-                return true;
-            } 
-            else {
-                x[`${nums[i]}`] = true;
-            }
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function hasDuplicate(nums) {
+    let x = {};
+    for (let i = 0; i < nums.length; i++) {
+        if (x[`${nums[i]}`]) {
+            return true;
+        } 
+        else {
+            x[`${nums[i]}`] = true;
         }
-        return false;
     }
+    return false;
 }
