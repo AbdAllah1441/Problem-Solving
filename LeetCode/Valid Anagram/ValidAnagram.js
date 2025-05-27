63be6e68bb69800830c395df84e99e0581af39e1
عبدالله مصطفى <abdullahmostafa9957@gmail.com>
2025-05-27 11:49:11 +0300

    Update ValidAnagram.js

--- LeetCode/Valid Anagram/ValidAnagram.js
+++ LeetCode/Valid Anagram/ValidAnagram.js
@@ -6,22 +6,18 @@
 var isAnagram = function (s, t) {
   let s_map = {};
   let t_map = {};
-  for (let i = 0; i < s.length; i++) {
+  let length = s.length > t.length ? s.length : t.length;
+  for (let i = 0; i < length; i++) {
     if (s_map[`${s[i]}`]) {
       s_map[`${s[i]}`] += 1;
     } else {
       s_map[`${s[i]}`] = 1;
     }
-  }
-  for (let i = 0; i < t.length; i++) {
     if (t_map[`${t[i]}`]) {
       t_map[`${t[i]}`] += 1;
     } else {
       t_map[`${t[i]}`] = 1;
     }
-  }
-  if (Object.keys(s_map).length !== Object.keys(t_map).length) {
-    return false;
   }
   let keys = Object.keys(s_map);
   for (let i = 0; i < keys.length; i++) {
@@ -29,5 +25,5 @@ var isAnagram = function (s, t) {
       return false;
     }
   }
-  return true;
+  return s_map;
 };
