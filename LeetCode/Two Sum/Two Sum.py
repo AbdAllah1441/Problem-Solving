09d05b42f81b1ffed407464d710f67ac6cf27fc4
عبدالله مصطفى <abdullahmostafa9957@gmail.com>
2025-05-28 03:28:52 +0300

    Update and rename Two Sum.py to TwoSum.js

--- LeetCode/Two Sum/Two Sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        x = len(nums)
-        for i in range(x):
-            j = i + 1
-            while j < x:
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-                j += 1
